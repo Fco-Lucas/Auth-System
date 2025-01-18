51da8679f645dd00794cea9456ceeb44323bbf76
Lucas <fcolucasmaia@gmail.com>
2025-01-17 21:26:14 -0300

    starter project

---
+++ src/main/java/com/lcsz/authSistem/AuthSistemApplication.java
@@ -0,0 +1,13 @@
+package com.lcsz.authSistem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSistemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSistemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lcsz/authSistem/AuthSistemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcsz.authSistem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSistemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
